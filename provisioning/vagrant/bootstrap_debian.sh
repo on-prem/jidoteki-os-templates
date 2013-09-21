7b851f8b90646384849a61e8ff4c373930c73d88
Alex Williams <awilliams@alexwilliams.ca>
2013-09-21 18:44:13 +0000

    Add bootstrap scripts for creating Vagrant boxes

---
+++ provisioning/vagrant/bootstrap_debian.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Bootstrap script for setting up Debian as a vagrant .box
+#
+# Copyright (c) 2013 Alex Williams, Unscramble <license@unscramble.jp>
+
+UNZIP=`which unzip`
+TAR=`which tar`
+
+fail_and_exit() {
+        echo "Provisioning failed"
+        exit 1
+}
+
+# Install some dependencies
+apt-get install python-dev python-pip && \
+pip install PyYAML Jinja2 || fail_and_exit
+
+pushd /root
+  # Extract ansible and install it
+  $TAR -zxvf v1.3.0.tar.gz || fail_and_exit
+  pushd ansible-1.3.0
+    # Install Ansible
+    make install && \
+    source hacking/env-setup || fail_and_exit
+  popd
+
+  # Extract public provisioning scripts
+  $UNZIP -o beta-v2.zip || fail_and_exit
+  pushd jidoteki-os-templates-beta-v2/provisioning/vagrant
+    # Run ansible in local mode
+    ansible-playbook vagrant.yml -i hosts || fail_and_exit
+  popd
+popd
+
+echo "Provisioning completed successfully"
+exit 0
---
+++ provisioning/vagrant/bootstrap_freebsd.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Bootstrap script for setting up FreeBSD as a vagrant .box
+#
+# Copyright (c) 2013 Alex Williams, Unscramble <license@unscramble.jp>
+
+UNZIP=`which unzip`
+TAR=`which tar`
+
+fail_and_exit() {
+        echo "Provisioning failed"
+        exit 1
+}
+
+# Install some dependencies
+pkg_add -r gmake -F && \
+pkg_add -r python -F && \
+pkg_add -r bash -F && \
+ln -sf /usr/local/bin/python /usr/bin/python && \
+pip install PyYAML Jinja2 || fail_and_exit
+
+cd /root
+  # Extract ansible and install it
+  $TAR -zxvf v1.3.0.tar.gz || fail_and_exit
+  cd ansible-1.3.0
+    # Install Ansible
+    gmake install && \
+    /usr/local/bin/bash hacking/env-setup || fail_and_exit
+  cd ..
+
+  # Extract public provisioning scripts
+  $UNZIP -o beta-v2.zip || fail_and_exit
+  cd jidoteki-os-templates-beta-v2/provisioning/vagrant
+    # Run ansible in local mode
+    chmod 644 hosts && \
+    ansible-playbook vagrant.yml -i hosts || fail_and_exit
+  cd ..
+cd ..
+
+echo "Provisioning completed successfully"
+exit 0
