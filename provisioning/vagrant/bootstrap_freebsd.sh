9e99012a808edb7893b1072a808764625b90bb3a
Alex Williams <awilliams@alexwilliams.ca>
2013-09-23 01:52:02 +0000

    Add libyaml for FreeBSD / ansible

--- provisioning/vagrant/bootstrap_freebsd.sh
+++ provisioning/vagrant/bootstrap_freebsd.sh
@@ -17,6 +17,7 @@ pkg_add -r gmake -F && \
 pkg_add -r python27 -F && \
 pkg_add -r bash -F && \
 pkg_add -r py27-pip -F && \
+pkg_add -r libyaml -F && \
 ln -sf /usr/local/bin/python /usr/bin/python && \
 pip install --use-mirrors PyYAML Jinja2 || fail_and_exit
 
@@ -35,11 +36,10 @@ cd /root
     # Run ansible in local mode
     chmod 644 hosts && \
     ansible-playbook vagrant.yml -i hosts || fail_and_exit
-  cd ..
+  cd /root
 
   # Cleanup
-  rm -rf v1.3.0.tar.gz ansible-1.3.0 beta-v2.zip jidoteki-os-templates-beta-v2 bootstrap_freebsd.sh || fail_and_exit
-  history -c
+  rm -rf v1.3.0.tar.gz ansible-1.3.0 beta-v2.zip jidoteki-os-templates-beta-v2 bootstrap_freebsd.sh
 cd ..
 
 echo "Provisioning completed successfully"
