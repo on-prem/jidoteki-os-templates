30abdd73a86f72868c984a3fb377579d8e27ae5b
Alex Williams <awilliams@alexwilliams.ca>
2014-09-28 17:53:19 +0000

    Ensure FreeBSD 10 has 'ports' and 'pkg' installed

--- provisioning/deps_freebsd10.sh
+++ provisioning/deps_freebsd10.sh
@@ -9,6 +9,9 @@ EOF
 )
 
 install_deps() {
+  cd /usr/ports/ports-mgmt/pkg
+  make -DBATCH install
+  pkg2ng
   pkg install -y $provisioner_pkgs && \
   ln -sf /usr/local/bin/python /usr/bin/python && \
   pip install --use-mirrors PyYAML Jinja2
