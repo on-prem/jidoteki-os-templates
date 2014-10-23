0d5daa037e09f3f884de344ccf3690c6be4680f2
Alex Williams <awilliams@alexwilliams.ca>
2014-10-23 12:15:11 +0000

    Add example update script which doesn't do anything

---
+++ provisioning/update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Example script for updating a virtual appliance (doesn't actually do anything)
+#
+# Copyright (c) 2013-2014 Alex Williams, Unscramble. MIT License.
+# http://unscramble.co.jp
+
+echo "Updating the server.."
+echo ""
+sleep 1
+echo "Installing packages.."
+echo ""
+sleep 2
+echo "Updating app.."
+echo ""
+sleep 1
+echo "Restarting services.."
+echo ""
+sleep 2
+echo 'All done!'
+
+exit 0
