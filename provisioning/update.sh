2e8135b2c1b03dc8a5cc632b49aff3b3b1ddec1f
Alex Williams <awilliams@alexwilliams.ca>
2014-10-30 09:06:37 +0000

    Make the 'update.sh' script more generic

--- provisioning/update.sh
+++ provisioning/update.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #
-# Example script for updating a virtual appliance (doesn't actually do anything)
+# Generic script for updating a virtual appliance
 #
 # Copyright (c) 2013-2014 Alex Williams, Unscramble. MIT License.
 # http://unscramble.co.jp
-
-echo "Updating the server.."
 
 set -u
 set -e
@@ -13,7 +11,5 @@ set -e
 ### Update Functions ###
 
 trap 'exit 127' INT
-
-echo 'All done!'
 
 exit 0
