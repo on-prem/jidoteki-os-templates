a84b640bb5fcb652fa40bd835a1d20087ac44abf
Alex Williams <awilliams@alexwilliams.ca>
2014-10-30 08:17:25 +0000

    Add update commands for debian and centos

--- provisioning/update_centos.sh
+++ provisioning/update_centos.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-#
-# Example script for updating a virtual appliance (only updates OS packages)
-#
-# Copyright (c) 2013-2014 Alex Williams, Unscramble. MIT License.
-# http://unscramble.co.jp
-#
-# CENTOS
-
-echo "Installing latest packages.."
-
-for i in `ls -1 *.rpm`; do rpm -ivh $i ; done
-
-echo 'All done!'
-
-exit 0
+mv *.rpm /opt/jidoteki/repos/yum/centos/RPMS/
+createrepo /opt/jidoteki/repos/yum/centos
+yum update -y --enablerepo=local
--- provisioning/update_debian.sh
+++ provisioning/update_debian.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-#
-# Example script for updating a virtual appliance (only updates OS packages)
-#
-# Copyright (c) 2013-2014 Alex Williams, Unscramble. MIT License.
-# http://unscramble.co.jp
-#
-# DEBIAN / UBUNTU
-
-echo "Installing latest packages.."
-
-for i in `ls -1 *.deb`; do dpkg -i $i ; done
-
-echo 'All done!'
-
-exit 0
+reprepro -b /opt/jidoteki/repos/apt/debian includedeb local *.deb
+apt-get update -o Dir::Etc::sourcelist="local.list"
+DEBIAN_FRONTEND=noninteractive apt-get upgrade --force-yes -y -o Dir::Etc::sourcelist="local.list"
